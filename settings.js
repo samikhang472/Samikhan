304454679f62775c742f1684012ee9a5145af4b0
samikhang472 <mrsamijani55@gmail.com>
2025-04-18 06:00:18 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'SAMI HACKING',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "WHATSAPP Bot",
+  botOwner: 'pathan', // Your name
+  ownerNumber: '923429753095', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
